71505aa061c507d2993ca49d61739609da34b41c
Austin Wright <austin@silentbreaksecurity.com>
2020-06-04 16:45:36 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import '@atlaskit/css-reset';
+import {DragDropContext} from 'react-beautiful-dnd';
+import {initialData} from './initial-data';
+import {Column} from './column';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [data, setData] = useState(initialData);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const onDragEnd = result => {
+    const {destination, source, draggableId} = result;
+
+    if(!destination) {
+      return;
+    }
+    if(
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = data.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    }
+    const newState = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id]: newColumn
+      }
+    }
+    setData(newState);
+  }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      {data.columnOrder.map(columnId => {
+        const column = data.columns[columnId];
+        const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
+        return <Column key={column.id} column={column} tasks={tasks}/>;
+      })}
+    </DragDropContext>
+  );
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
